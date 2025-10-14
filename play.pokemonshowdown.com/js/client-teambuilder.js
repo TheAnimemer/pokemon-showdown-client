a43af73b509728e0622a4eae656e84d59daa5f33
TheAnimemer <146009953+TheAnimemer@users.noreply.github.com>
2025-10-13 20:48:55 -0400

    hard-coded set redirect

--- play.pokemonshowdown.com/js/client-teambuilder.js
+++ play.pokemonshowdown.com/js/client-teambuilder.js
@@ -1787,7 +1787,7 @@
 			// We fetch this as 'text' and JSON.parse it ourserves in order to have consistent behavior
 			// between the localdev CORS helper and the real jQuery.get function, which would already parse
 			// this into an object based on the content-type header.
-			$.get('https://' + Config.routes.client + '/data/sets/' + format + '.json', {}, function (data) {
+			$.get('https://play.tanoso.online/data/sets/' + format + '.json', {}, function (data) {
 				try {
 					self.smogonSets[format] = JSON.parse(data);
 				} catch (e) {
